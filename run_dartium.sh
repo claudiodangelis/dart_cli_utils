b4a92e9c0ec5ea2265bbf23058059ea180ff7881
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-08-07 13:33:48 +0200

    Added run_dartium.sh

---
+++ run_dartium.sh
@@ -0,0 +1,9 @@
+function run_dartium {
+    /home/dawson/dart/chromium/chrome \
+        --user-data-dir=/home/dawson/.dartium \
+        --enable-experimental-web-platform-features \
+        --enable-html-imports \
+        --no-first-run \
+        --no-default-browser-check \
+        --no-process-singleton-dialog
+}
